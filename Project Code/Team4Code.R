265d674d7c6c7e754f6a71389d19e79848f7256e
Evi Prousanidou <evi@Evis-MBP-2.ht.home>
2025-04-28 15:41:34 -0400

    fixed weird function format

--- Project Code/Team4Code.R
+++ Project Code/Team4Code.R
@@ -18,12 +18,11 @@ ff_path <- file.path(getwd(), "data/ff_daily.csv")
 ff <- read_csv(ff_path)
 
 # Convert Date to date
-ff <- ff |> mutate(Date = as.Date(as.character(Date), format = "%Y%m%d"))
-ff <- ff |> relocate(Date)  #move Date to first column if needed
+ff <- mutate(ff, Date = as.Date(as.character(Date), format = "%Y%m%d"))
+ff <- relocate(ff, Date)
 
 # Divide all columns except Date by 100
-ff <- ff |> mutate(across(-Date, ~ .x / 100))
-# Check
+ff <- mutate(ff, across(-Date, ~ .x / 100))
 print(head(ff))
 
 
@@ -33,75 +32,67 @@ returns_path <- file.path(getwd(), "returns.csv")
 returns <- read_csv(returns_path)
 print(head(returns))
 
-returns <- returns |> select(-GEV, -SOLV, -SW)
+returns <- select(returns,-GEV,-SOLV,-SW)
 print(head(returns))
 
 # pivot to have date|symbol|return
-returns <- returns |> 
-  pivot_longer(
-    cols = -Date,
-    names_to = "symbol",
-    values_to = "ret"
-  ) |> 
-  drop_na()
+returns <- pivot_longer(returns, cols = -Date, names_to = "symbol", values_to = "ret")
+returns <- drop_na(returns)
+returns <- filter(returns, ret != 0)
+
 head(returns)
 
 # Merge returns with Fama-French factors
-returns_with_rf <- returns |> 
-  left_join(ff, by = "Date")
+returns_with_rf <- left_join(returns, ff, by = "Date")
 
 #Calculate excess return
-returns_with_rf <- returns_with_rf |> 
-  mutate(excess_ret = ret - RF)
-head(returns_with_rf)
+returns_with_rf <- mutate(returns_with_rf, excess_ret = ret - RF)
 
 # Create a 'month' variable
-returns_with_rf <- returns_with_rf |> 
-  mutate(month = floor_date(Date, unit = "month"))
+returns_with_rf <- mutate(returns_with_rf, month = floor_date(Date, unit = "month"))
 
-# For each stock and month, get the last trading day's excess return
-monthly_excess <- returns_with_rf |>
-  group_by(symbol, month) |>
-  slice_tail(n = 1) |>  # get the last row in each month
-  ungroup()
+# For each stock and month, get the cummulative monthly excess return
+
+'''monthly_excess <- group_by(returns_with_rf, symbol, month)
+monthly_excess <- slice_tail(monthly_excess, n = 1)  # get the last row in each month
+monthly_excess <- ungroup(monthly_excess)'''
+
+monthly_excess <- group_by(returns_with_rf, symbol, month)
+monthly_excess <- summarise(monthly_excess,
+                            excess_ret = prod(1 + excess_ret) - 1,
+                            .groups = "drop")
 
 head(monthly_excess)
 
 ####################### MOMENTUM SIGNAL #######################
-
 install.packages("slider")
 library(slider)
 
-# We already have 'monthly_excess' with symbol, month, excess_ret
-
-monthly_excess <- monthly_excess |> arrange(symbol, month)
+# order monthly excess by symbol and month
+monthly_excess <- arrange(monthly_excess, symbol, month)
 
 # Calculate rolling 12 months cumulative return (T-2 to T-13)
-momentum_signal <- monthly_excess |> 
-  group_by(symbol) |> 
-  mutate(
-    cum_return = slide_index_dbl(
-      .x = excess_ret,
-      .i = month,
-      .f = ~ prod(1 + .x) - 1,
-      .before = 11,  # 12 months total
-      .complete = TRUE
-    )
-  ) |> 
-  ungroup()
+momentum_signal <- group_by(monthly_excess, symbol)
+momentum_signal <- mutate(momentum_signal,
+                          cum_return = slide_index_dbl(
+                            .x = excess_ret,
+                            .i = month,
+                            .f = ~ prod(1 + .x) - 1,
+                            .before = 11,
+                            .complete = TRUE
+                          ))
+momentum_signal <- ungroup(momentum_signal)
 
-momentum_signal <- momentum_signal |> 
-  group_by(symbol) |> 
-  mutate(formation_month = lead(month, 1)) |> 
-  ungroup()
+momentum_signal <- group_by(momentum_signal, symbol)
+momentum_signal <- mutate(momentum_signal, formation_month = lead(month, 1))
+momentum_signal <- ungroup(momentum_signal)
 
-momentum_signal <- momentum_signal |> 
-  select(symbol, formation_month, cum_return) |> 
-  filter(!is.na(formation_month))
+momentum_signal <- select(momentum_signal, symbol, formation_month, cum_return)
+momentum_signal <- filter(momentum_signal, !is.na(formation_month))
+
+momentum_signal <- filter(momentum_signal, !is.na(cum_return))  
 
 head(momentum_signal)
-
-
 
 '''# Rank and assign weights
 top3_weights <- momentum_signal |> 
@@ -118,28 +109,27 @@ head(top3_weights)'''
 
 
 # Rank and assign weights for top 10%
-top10pct_weights <- momentum_signal |> 
-  group_by(formation_month) |> 
-  arrange(desc(cum_return)) |> 
-  mutate(
-    rank = row_number(),
-    n_stocks = n(),  # total number of stocks in that month
-    threshold = floor(0.10 * n_stocks),  # top 10% (round down)
-    weight = if_else(rank <= threshold, 1 / threshold, 0)
-  ) |> 
-  ungroup() |> 
-  select(symbol, formation_month, cum_return, rank, weight)
+top10pct_weights <- group_by(momentum_signal, formation_month)
+top10pct_weights <- arrange(top10pct_weights, desc(cum_return))
+top10pct_weights <- mutate(top10pct_weights,
+                            rank = row_number(),
+                            n_stocks = n(),  # total number of stocks in that month
+                            threshold = floor(0.10 * n_stocks),  # top 10% (round down)
+                            weight = if_else(rank <= threshold, 1 / threshold, 0))
+top10pct_weights <- ungroup(top10pct_weights)
+top10pct_weights <- select(top10pct_weights, symbol, formation_month, cum_return, rank, weight)
 
 head(top10pct_weights)
 
-# # stocks are selected each month
-top10pct_selected_count <- top10pct_weights |> 
-  filter(weight > 0) |> 
-  group_by(formation_month) |> 
-  summarise(n_selected = n()) |> 
-  arrange(formation_month)
+# Stocks selected each month
+top10pct_selected_count <- filter(top10pct_weights, weight > 0)
+top10pct_selected_count <- group_by(top10pct_selected_count, formation_month)
+top10pct_selected_count <- summarise(top10pct_selected_count, n_selected = n())
+top10pct_selected_count <- arrange(top10pct_selected_count, formation_month)
 
 print(top10pct_selected_count)
+print(arrange(top10pct_selected_count, desc(formation_month)))
+
 
 
 
